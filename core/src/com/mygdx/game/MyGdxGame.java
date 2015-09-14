74b61395a6bb0aaf4828a7269936d35cd40d1889
Patrick Brady <pj.brady@comcast.net>
2015-09-14 09:34:57 -0700

    Hello. I would like to change my life around from tye d8ms iut ahad nc00ep,e
    it is a great color now

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -18,7 +18,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0.34f, 0.98f, 0.11f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
